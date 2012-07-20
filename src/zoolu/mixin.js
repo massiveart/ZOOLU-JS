0c5ef5a3ed55eef948f70df2e9cde1951e454976
Marcel Moosbrugger <marcel.moosbrugger@bws.ac.at>
2012-07-20 10:39:31 +0200

    IE7 if args are undefined (error)
    Set args-array to an empty array, if no args given.

--- src/zoolu/mixin.js
+++ src/zoolu/mixin.js
@@ -82,9 +82,12 @@
          */
         trigger: function(event, args) {
             if (!!this.listeners[event]) {
+            	if(args == undefined){
+            		args = [];
+            	}
                 for (var i = -1, length = this.listeners[event].length; ++i < length;) {
                     // execute in the global scope (window), though this could also be customized
-                    this.listeners[event][i].apply(window, args);
+                	this.listeners[event][i].apply(window, args);
                 }
             }
         },
